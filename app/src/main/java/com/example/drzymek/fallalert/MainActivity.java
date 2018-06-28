9c2a925f815620a84876135ed174db3c958e2d31
DartDrzymek <kacper.drzymala333@gmial.com>
2018-06-28 19:32:36 +0200

    new: sending sms function

--- app/src/main/java/com/example/drzymek/fallalert/MainActivity.java
+++ app/src/main/java/com/example/drzymek/fallalert/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.drzymek.fallalert;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
+    private static Integer MY_PERMISSIONS_REQUEST_READ_CONTACTS=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.SEND_SMS},
+                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        System.out.println(MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        sendSMS("501815074","dupa kupa");
     }
+
+    private void sendSMS(String phoneNumber, String message)
+    {
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, SMS.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
+    }
+    
 }
